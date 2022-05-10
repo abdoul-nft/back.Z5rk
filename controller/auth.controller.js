2d17070eb6d2738e9056b5fe207d1ff4aca9f267
abdoul-nft <abdoul.nft@gmail.com>
2022-05-10 11:13:16 +0200

    wip

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,15 +1,22 @@
-
-    const bcrypt = require('bcryptjs');
     const Models = require('../models/index');
 
     const addAccompte = req => {
         return new Promise( (resolve, reject) => {
             if(!req.params.wallet_address) return reject( {'error': 'token address is required'})
-            Models.user.create({ 'wallet_address': req.params.wallet_address })
-            .then( user => {
-                resolve(user) 
+            Models.user.findOne({ 'wallet_address': req.params.wallet_address }, (err, user) => {
+                if(err) reject(err)
+                else{
+                    if (!user) {
+                        Models.user.create({ 'wallet_address': req.params.wallet_address })
+                        .then( data => {
+                            resolve(data) 
+                        })
+                        .catch( err => reject(err) )
+                    }else {
+                        reject( {'error': 'user alerady exist'})
+                    }
+                }
             })
-            .catch( err => reject(err) )
         })
     }
 
@@ -26,8 +33,12 @@
 
     const updateOne = req => {
         return new Promise((resolve, reject) => {
+            const {email, username, bio, twitter_username, instagram_username} = req.body
             if(!req.params.wallet_address) return reject( {'error': 'token address is required'})
-            Models.user.updateOne( { 'wallet_address': req.params.wallet_address }, req.body, (err, user) => {
+            Models.user.updateOne( 
+                { 'wallet_address': req.params.wallet_address }, 
+                {'email': email, 'username': username, 'bio': bio, 'twitter_username': twitter_username, 'instagram_username': instagram_username}, 
+                (err, user) => {
                 return err
                 ? reject(err)
                 : resolve(user) 
